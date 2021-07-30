27fcb32760d15f53bbd517f2d355748febde9807
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-07-30 19:09:30 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDp6c0DQvmPQ_dQoIxVFIkeTLw5lgtgQAQ",
+    authDomain: "speed-typing-game-9329f.firebaseapp.com",
+    projectId: "speed-typing-game-9329f",
+    storageBucket: "speed-typing-game-9329f.appspot.com",
+    messagingSenderId: "640260155926",
+    appId: "1:640260155926:web:5eb99689bb76a3be6fa337",
+    measurementId: "G-CZHMF37C3E"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
--- script.js
+++ script.js
@@ -8,9 +8,18 @@ const wordInput=document.querySelector('#input');
 const qouteWord=document.querySelector('#current-word');
 const scoreDisplay=document.querySelector('#score');
 const timeDisplay=document.querySelector('#time');
-const button=document.querySelector('#start');
+const button=document.getElementById("startB")
 const alert=document.querySelector('#alert');
-button.addEventListener('click',init);
+const highestScore=document.getElementById('highest');
+
+
+var signupButton=document.getElementById("Signin");
+var loginDiv=document.getElementById("loginDiv");
+var alertt=document.getElementById("alert");
+var db;
+var uid ;
+
+db = firebase.firestore();
 
 const words=['hat','the','great' ,'Maratha', 'queen', 'Lokmata' ,'cow','crazy','express',
 'crack','creative','examine',
@@ -29,8 +38,10 @@ const words=['hat','the','great' ,'Maratha', 'queen', 'Lokmata' ,'cow','crazy','
 'action','active','laughter','magic','master','space','definition'];
 
 
+
 timeDisplay.innerHTML=time;
 function init(){
+  
  time=60;
  score=0;
  scoreDisplay.innerHTML=score; 
@@ -68,6 +79,10 @@ key= setInterval(countDown,1000);
      isPlaying=true;
      }
      scoreDisplay.innerHTML=score; 
+     if(score>highestScore.innerText){
+       highestScore.innerText=score;
+       db.collection("score").doc(uid).update("firebaseScore",score);
+     }
  }
  
  function matchWords(){
@@ -75,6 +90,110 @@ key= setInterval(countDown,1000);
        return true;
      }
      else{
+         verdict.innerHTML="";
          return false;
      }
  }
+
+
+
+ 
+function changeSigninDiv(){
+  document.getElementById("email").value="";
+  document.getElementById("password").value="";
+  document.getElementById("Signin").value="SIGN UP";
+  document.getElementById("Sign").className="inactive";
+  loginDiv.className="active underlineHover"
+}
+function changeSignupDiv(){
+  document.getElementById("email").value="";
+  document.getElementById("password").value="";
+  document.getElementById("Signin").value="SIGN IN";
+  document.getElementById("Sign").className="active";
+  loginDiv.className="inactive underlineHover"
+}
+
+function myFunc(){
+    var email=document.getElementById("email").value;
+     var password=document.getElementById("password").value;
+   //  window.alert(email+" "+password);
+  
+    if(signupButton.value==="SIGN UP"){
+   // console.log("jdfjf");
+     firebase.auth().createUserWithEmailAndPassword(email, password).then(
+       function(){
+       // window.location.href = "game.html";
+       document.getElementById("login").style.display="none";
+         document.getElementById("game").style.display="block";
+        var user = firebase.auth().currentUser;
+  if(user){
+  uid=user.email;
+  //window.alert(uid);
+  }
+  
+  db.collection("score").doc(uid).set({
+    firebaseScore:0
+    });
+    document.getElementById("startB").addEventListener('click',init);
+       }
+     ).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      // window.alert(errorMessage);
+       alertt.className="alert alert-warning alert-dismissible fade show";
+      });
+    }
+    else{
+     
+      firebase.auth().signInWithEmailAndPassword(email, password).then(
+        function(){
+         // console.log("djf");
+         document.getElementById("login").style.display="none";
+         document.getElementById("game").style.display="block";
+         document.getElementById('highest').innerText="";
+         var user = firebase.auth().currentUser;
+         if(user){
+          uid=user.email;
+          //window.alert(uid);
+          }
+         db.collection("score")
+         .doc(uid)
+         .get()
+         .then((docRef) => { document.getElementById('highest').innerText=docRef.data().firebaseScore });
+         
+          document.getElementById("startB").addEventListener('click',init);
+          
+        }
+      ).catch(function(error) {
+        // Handle Errors here.
+      //   var errorCode = error.code;
+        var errorMessage = error.message;
+      //  // console.log(errorCode);
+       // window.alert(errorMessage);
+      alertt.className="alert alert-warning alert-dismissible fade show";
+      //  console.log(errorMessage);
+    });
+    }
+   
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+      
+        
+     } else {
+        // User is signed out
+        // ...
+      }
+    });
+}
+
+  function logoutUser(){
+   
+    document.getElementById("login").style.display="block";
+         document.getElementById("game").style.display="none";
+         document.getElementById("email").value="";
+         document.getElementById("password").value="";
+         firebase.auth().signOut();
+  }
